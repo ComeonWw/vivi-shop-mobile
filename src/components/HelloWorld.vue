180322a7e913153c0ecc77a7b9de0a25c7102554
vivi <3467085831@qq.com>
2022-03-06 16:36:09 +0800

    配置代码检验规则eslint

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,7 +3,10 @@ import { ref } from 'vue'
 
 // defineProps在组件间传值的时候使用，新版本的vue不需要引入defineProps
 defineProps({
-  msg: String
+  msg: {
+    type: String,
+    required: true
+  }
 })
 
 const count = ref(0)
@@ -14,20 +17,37 @@ const count = ref(0)
 
   <p>
     Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
+    <a
+      href="https://code.visualstudio.com/"
+      target="_blank"
+    >VSCode</a>
     +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+    <a
+      href="https://github.com/johnsoncodehk/volar"
+      target="_blank"
+    >Volar</a>
   </p>
 
   <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
+    <a
+      href="https://vitejs.dev/guide/features.html"
+      target="_blank"
+    >
       Vite Documentation
     </a>
     |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+    <a
+      href="https://v3.vuejs.org/"
+      target="_blank"
+    >Vue 3 Documentation</a>
   </p>
 
-  <button type="button" @click="count++">count is: {{ count }}</button>
+  <button
+    type="button"
+    @click="count++"
+  >
+    count is: {{ count }}
+  </button>
   <p>
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
