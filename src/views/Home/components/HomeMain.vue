620fcd726c939e8a44301dffbc9e46db50fb5cb2
vivi <3467085831@qq.com>
2022-03-12 12:20:16 +0800

    轮播区域

--- src/views/Home/components/HomeMain.vue
+++ src/views/Home/components/HomeMain.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="home-main">
-    主题内容
+    <!-- 区域1： 轮播图 -->
+    <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+      <van-swipe-item>1</van-swipe-item>
+      <van-swipe-item>2</van-swipe-item>
+      <van-swipe-item>3</van-swipe-item>
+      <van-swipe-item>4</van-swipe-item>
+    </van-swipe>
   </div>
 </template>
 
@@ -10,6 +16,9 @@
 
 <style lang="scss" scoped>
 .home-main {
-  padding: 50px 0;
+  // 区域1：轮播图
+  .my-swipe img {
+    width: 100%;
+  }
 }
 </style>
