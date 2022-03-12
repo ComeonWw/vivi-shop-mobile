12f49d88c318c1b8429c4ee52732097a530e33e8
vivi <3467085831@qq.com>
2022-03-12 12:13:30 +0800

    主题内容组件

--- src/views/Home/components/HomeMain.vue
+++ src/views/Home/components/HomeMain.vue
@@ -3,3 +3,13 @@
     主题内容
   </div>
 </template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.home-main {
+  padding: 50px 0;
+}
+</style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -3,7 +3,7 @@
   <home-nav></home-nav>
 
   <!-- 主体区域 -->
-  <home-main></home-main>
+  <home-main class="home-main"></home-main>
 
   <!-- 公共底部 -->
   <layout-footer></layout-footer>
@@ -14,3 +14,9 @@ import HomeNav from './components/HomeNav.vue'
 import HomeMain from './components/HomeMain.vue'
 import LayoutFooter from '@/components/LayoutFooter.vue';
 </script>
+
+<style lang="scss" scoped>
+.home-main {
+  background-color: #f7f7f7;
+}
+</style>
