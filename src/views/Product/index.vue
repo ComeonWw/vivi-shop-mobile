895b982b4f63f5ca2e545c04ba4d080f8b6166c4
vivi <3467085831@qq.com>
2022-03-14 16:10:06 +0800

    顶部导航+轮播图区域

--- src/views/Product/index.vue
+++ src/views/Product/index.vue
@@ -1,5 +1,22 @@
 <template>
-
+  <van-nav-bar left-arrow fixed></van-nav-bar>
+  <van-tabs sticky scrollspy>
+    <!-- 商品 -->
+    <van-tab title="商品">
+      <!-- 1：轮播 -->
+      <van-swipe class="my-swipe" :autoplay="3000" height="375">
+        <van-swipe-item
+          v-for="(index, item) in sliderImage"
+          :key="index"
+        >
+          <img :src="item">
+        </van-swipe-item>
+      </van-swipe>
+    </van-tab>
+    <van-tab title="评价">内容 2</van-tab>
+    <van-tab title="推荐">内容 3</van-tab>
+    <van-tab title="详情">内容 4</van-tab>
+  </van-tabs>
 </template>
 
 <script setup>
@@ -8,6 +25,7 @@ import { useRouter } from 'vue-router'
 // -----请求商品数据------
 import { getProductDetails } from '@/api/product'
 import { ref } from '@vue/reactivity';
+import { computed } from '@vue/runtime-core';
 
 //@ts-check
 const router = new useRouter()
@@ -31,10 +49,46 @@ const initProductDetails = async () => {
     })
   }
   productDetails.value = data.data
+  console.log(data.data)
 }
 initProductDetails()
+
+// =====商品数据处理=====
+// 1 商品详情信息
+const storeInfo = computed(() => productDetails.value.storeInfo)
+// 1.1 轮播图数据
+const sliderImage = computed(() => storeInfo.value?.slider_image)
+
 </script>
 
 <style lang="scss" scoped>
+// 为了避免问题， 添加fixed样式权重
+.van-nav-bar {
+  position: fixed !important;
+}
 
+// 顶部tabs的标题部分
+:deep(.van-tabs__wrap) {
+  position: fixed;
+  top: 0;
+  z-index: 999;
+  // 常规居中设置
+  left: 50%;
+  transform: translateX(-50%);
+  // 设置宽度
+  width: 80%;
+  .van-tab {
+    line-height: center !important;
+  }
+}
+
+// 底部主题内容容器
+:deep(.van-tabs__content) {
+  margin-top: 60px;
+}
+
+// 轮播图样式
+.van-swipe-item img {
+  width: 375px;
+}
 </style>
